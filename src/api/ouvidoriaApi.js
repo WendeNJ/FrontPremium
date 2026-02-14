e1ee093ab7d6e827e98a3ecc1dbd33c734f09465
Wenderson Carvalho <Wendersonmotarauj@gmail.com>
2026-02-14 15:10:19 -0300

    Set default API base URL for development

--- src/api/ouvidoriaApi.js
+++ src/api/ouvidoriaApi.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8081"; // ✅
+
 
 const api = axios.create({
   baseURL: API_BASE_URL,
